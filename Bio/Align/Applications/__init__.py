19f8aba8e69b139278f9dc60dea072e86f0df63a
Cymon J. Cox <cy@cymon.org>
2009-04-25 22:22:31 +0100

    Updated __init__.py to import DALIGN command line

--- Bio/Align/Applications/__init__.py
+++ Bio/Align/Applications/__init__.py
@@ -5,3 +5,4 @@ Pull in Application command lines
 from _Muscle import MuscleCommandline
 from _Mafft import MafftCommandline
 from _Prank import PrankCommandline
+from _Dialign import DialignCommandline
