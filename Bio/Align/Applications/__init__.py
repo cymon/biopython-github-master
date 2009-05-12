2ed108d0f09dca701bb1258a063a9e1b2ccd2da8
Cymon J. Cox <cymon@chara.localdomain>
2009-05-12 14:21:57 +0100

    Probcons command line and unittests

--- Bio/Align/Applications/__init__.py
+++ Bio/Align/Applications/__init__.py
@@ -8,3 +8,4 @@ from _Clustalw import ClustalwCommandline
 from _Prank import PrankCommandline
 from _Mafft import MafftCommandline
 from _Dialign import DialignCommandline
+from _Probcons import ProbconsCommandline
